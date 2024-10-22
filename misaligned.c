73c52f2736cccc0e102c0f2ce9cd6bd9a2dacfc8
Sharath-S-T <sharath.st123@gmail.com>
2024-10-22 15:11:52 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
-int printColorMap() {
+// Mocking the expected output
+const char* expectedOutput[25] = {
+    "0 | White | Blue\n",
+    "1 | White | Orange\n",
+    "2 | White | Green\n",
+    "3 | White | Brown\n",
+    "4 | White | Slate\n",
+    "5 | Red | Blue\n",
+    "6 | Red | Orange\n",
+    "7 | Red | Green\n",
+    "8 | Red | Brown\n",
+    "9 | Red | Slate\n",
+    "10 | Black | Blue\n",
+    "11 | Black | Orange\n",
+    "12 | Black | Green\n",
+    "13 | Black | Brown\n",
+    "14 | Black | Slate\n",
+    "15 | Yellow | Blue\n",
+    "16 | Yellow | Orange\n",
+    "17 | Yellow | Green\n",
+    "18 | Yellow | Brown\n",
+    "19 | Yellow | Slate\n",
+    "20 | Violet | Blue\n",
+    "21 | Violet | Orange\n",
+    "22 | Violet | Green\n",
+    "23 | Violet | Brown\n",
+    "24 | Violet | Slate\n"
+};
+
+void checkOutput(const char* printedOutput) {
+    int i = 0;
+    char buffer[50];
+    char* outputPtr = (char*)printedOutput;
+
+    for (i = 0; i < 25; i++) {
+        // Extract one line of output at a time from printedOutput
+        sscanf(outputPtr, "%[^\n]\n", buffer);
+        outputPtr += strlen(buffer) + 1;  // Move to next line in output
+
+        // Compare the expected output
+        assert(strcmp(buffer, expectedOutput[i]) == 0 && "Output does not match the expected combination");
+    }
+}
+
+int printColorMap(char* printedOutput) {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
@@ -14,8 +59,10 @@ int printColorMap() {
 }
 
 int main() {
+    char printedOutput[2000] = {0};    
     int result = printColorMap();
     assert(result == 25);
+    checkOutput(printedOutput);  // This will validate the correctness of the output
     printf("All is well (maybe!)\n");
     return 0;
 }
